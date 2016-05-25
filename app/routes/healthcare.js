8c2d67ef77a0df29127ae494abfeaefa22ce85f7
Kenneth Bergquist <bergquist.kenneth@gmail.com>
2016-05-25 15:35:29 -0400

    Active links

--- app/routes/healthcare.js
+++ app/routes/healthcare.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	  model() {
+  model() {
     return this.store.query('tweet',  {topic: 'healthcare' });
-  }
+  },
+	topic: 'healthcare'
 });
